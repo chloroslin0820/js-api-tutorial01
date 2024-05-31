3390c147318953474b3270b46cc4221db570a19a
chloros <chloros.lin@gmail.com>
2024-05-31 11:11:03 +0800

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,66 @@
+const pokemonCount = 251;
+let pokedex = {};
+
+window.onload = async () => {
+    for (let i = 1; i <= pokemonCount; i++) {
+        await getPokemon(i);
+
+        let pokemonElement = document.createElement("div");
+        pokemonElement.id = i;
+        pokemonElement.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase();
+        pokemonElement.classList.add("pokemon-name");
+        pokemonElement.addEventListener('click', () => refreshPokemon(i));
+        document.getElementById("pokemon-list").append(pokemonElement);
+    };
+
+    document.getElementById("pokemon-description").innerText = pokedex[1]["description"];
+};
+
+const getPokemon = async (num) => {
+    let url = 
+        "https://pokeapi.co/api/v2/pokemon/" + num.toString();
+
+    try {
+
+        let res = await fetch(url);
+        let pokemonData = await res.json();
+
+        let pokemonName = pokemonData["name"];
+        let pokemonType = pokemonData["types"];
+        let pokemonImg = pokemonData["sprites"]["front_default"];
+
+        res = await fetch(pokemonData["species"]["url"]);
+        let pokemonDescData = await res.json();
+        pokemonDesc = pokemonDescData["flavor_text_entries"][9]["flavor_text"];
+
+        pokedex[num] = 
+            {
+                "name" : pokemonName, 
+                "img" : pokemonImg, 
+                "types" : pokemonType, 
+                "description" : pokemonDesc
+            };
+
+    } catch (error) {
+        console.error(`Error fetching or parsing Pokemon data: ${error}`);
+    };
+};
+
+const refreshPokemon = (id) => {
+    document.getElementById("pokemon-img").src = pokedex[id]["img"];
+    document.getElementById("pokemon-description").innerText = pokedex[id]["description"];
+
+    let typesDiv = document.getElementById("pokemon-types");
+    while (typesDiv.firstChild) {
+        typesDiv.firstChild.remove();
+    };
+
+    let types = pokedex[id]["types"];
+    types.forEach(typeInfo => {
+        let typeElement = document.createElement("span");
+        typeElement.innerText = typeInfo["type"]["name"].toUpperCase();
+        typeElement.classList.add("type-box");
+        typeElement.classList.add(typeInfo["type"]["name"]);
+        typesDiv.append(typeElement);       
+    });
+};
